226ec638ed8040eaf6fe88d4fc8f995c4bcf3b09
Bilgecan Sen <bilgecan.sen@gmail.com>
2025-02-28 11:49:12 -0500

    stylistic changes to some scripts

--- scripts/forecast_growth.R
+++ scripts/forecast_growth.R
@@ -1,4 +1,3 @@
-
 library(MCMCvis)
 library(foreach)
 library(tidyverse)
@@ -16,21 +15,22 @@ sites_adpe_sf <- mapppdr::sites_sf %>%
 
 data_pop <- readRDS("data/data_pop_adpe.rds")
 
-# Parameter chains 
+# Parameter chains
 res_pop <- read_rds("data/results_pop_adpe.rds")
 
-param_chains <- MCMCchains(res_pop, 
-                            params = c("beta0", "beta", 
-                                       "sigma_site", "mu_sigma"))
+param_chains <- MCMCchains(
+  res_pop,
+  params = c("beta0", "beta", "sigma_site", "mu_sigma")
+)
 
 idx <- sample(1:nrow(param_chains), 100)
 
-b0 <- param_chains[idx,"beta0"]
-b1 <- param_chains[idx,"beta[1]"]
-b2 <- param_chains[idx,"beta[2]"]
+b0 <- param_chains[idx, "beta0"]
+b1 <- param_chains[idx, "beta[1]"]
+b2 <- param_chains[idx, "beta[2]"]
 
-s_s <- param_chains[idx,"sigma_site"]
-s_r <- rtruncnorm(100, a = 0, mean = 0, sd = param_chains[idx,"mu_sigma"])
+s_s <- param_chains[idx, "sigma_site"]
+s_r <- rtruncnorm(100, a = 0, mean = 0, sd = param_chains[idx, "mu_sigma"])
 
 # Forecast and hindcast
 env <- readRDS("data/data_coupled_normalized_adpe.rds")
@@ -38,24 +38,25 @@ idx_s <- which(!rownames(env[[1]]) %in% sites_adpe$site_id)
 
 ## remove sites not in mapppd
 for (i in 1:length(env)) {
-  env[[i]] <- env[[i]][-idx_s,]
+  env[[i]] <- env[[i]][-idx_s, ]
 }
 
-e_s <- rnorm(100*nrow(env[[1]]), 0, s_s) %>%
+e_s <- rnorm(100 * nrow(env[[1]]), 0, s_s) %>%
   matrix(nrow = nrow(env[[1]]), ncol = 100)
 
-e_r <- rnorm(100*nrow(env[[1]])*ncol(env[[1]]), 0, s_r) %>%
+e_r <- rnorm(100 * nrow(env[[1]]) * ncol(env[[1]]), 0, s_r) %>%
   array(dim = c(nrow(env[[1]]), ncol(env[[1]]), 100))
 
-r <- foreach(i = 1:length(env)) %do% {
-  
-  x <- env[[i]]
-  
-  foreach(k = 1:nrow(x)) %:% 
-    foreach(h = 1:100, .combine = "rbind") %do% {
-      b0[h] + b1[h]*x[k,] + b2[h]*(x[k,]^2) + e_r[k,,h] + e_s[k,h]
-    }
-}
+r <- foreach(i = 1:length(env)) %do%
+  {
+    x <- env[[i]]
+
+    foreach(k = 1:nrow(x)) %:%
+      foreach(h = 1:100, .combine = "rbind") %do%
+      {
+        b0[h] + b1[h] * x[k, ] + b2[h] * (x[k, ]^2) + e_r[k, , h] + e_s[k, h]
+      }
+  }
 
 # Save results in csv
 folder <- "growth_projections"
@@ -63,12 +64,10 @@ dir.create(folder)
 
 sites <- unique(row.names(env[[1]]))
 for (i in 1:length(sites)) {
-  
   site_folder <- paste(folder, sites[i], sep = "/")
   dir.create(site_folder)
-  
+
   for (k in 1:50) {
-    
     site_file <- paste(site_folder, "/", k, ".csv", sep = "")
     write.csv(r[[k]][[i]], site_file)
   }
@@ -80,37 +79,38 @@ for (i in 1:length(sites)) {
 # Mean sea ice across 50 members
 env_arr <- array(dim = c(nrow(env[[1]]), ncol(env[[1]]), 50))
 for (i in 1:50) {
-  env_arr[,,i] <- env[[i]]
+  env_arr[,, i] <- env[[i]]
 }
 
-env_mean <- apply(env_arr, c(1,2), mean)
+env_mean <- apply(env_arr, c(1, 2), mean)
 rownames(env_mean) <- rownames(env[[1]])
 
 # Parameter means
 params_mean <- MCMCsummary(res_pop, params = c("beta0", "beta"))
 
-b0_mean <- params_mean[1,1]
-b1_mean <- params_mean[2,1]
-b2_mean <- params_mean[3,1]
+b0_mean <- params_mean[1, 1]
+b1_mean <- params_mean[2, 1]
+b2_mean <- params_mean[3, 1]
 
 # Only parameter uncertainty
-r_param <- foreach(k = 1:nrow(env_mean)) %:% 
-    foreach(h = 1:100, .combine = "rbind") %do% {
-      b0[h] + b1[h]*env_mean[k,] + b2[h]*(env_mean[k,]^2)
-    }
+r_param <- foreach(k = 1:nrow(env_mean)) %:%
+  foreach(h = 1:100, .combine = "rbind") %do%
+  {
+    b0[h] + b1[h] * env_mean[k, ] + b2[h] * (env_mean[k, ]^2)
+  }
 
 for (i in 1:length(r_param)) {
   colnames(r_param[[i]]) <- colnames(env[[1]])
 }
 
 # Only process uncertainty
-e_s2 <- rnorm(100*nrow(env[[1]]), 0, mean(s_s)) %>%
+e_s2 <- rnorm(100 * nrow(env[[1]]), 0, mean(s_s)) %>%
   matrix(nrow = nrow(env[[1]]), ncol = 100)
 
-r_proc_s <- foreach(k = 1:nrow(env_mean)) %:% 
-  foreach(h = 1:100, .combine = "rbind") %do% {
-    b0_mean + b1_mean*env_mean[k,] + b2_mean*(env_mean[k,]^2) + 
-      e_s2[k,h]
+r_proc_s <- foreach(k = 1:nrow(env_mean)) %:%
+  foreach(h = 1:100, .combine = "rbind") %do%
+  {
+    b0_mean + b1_mean * env_mean[k, ] + b2_mean * (env_mean[k, ]^2) + e_s2[k, h]
   }
 
 for (i in 1:length(r_proc_s)) {
@@ -118,29 +118,32 @@ for (i in 1:length(r_proc_s)) {
 }
 
 # Only climate uncertainty
-r_clim <- 
-  foreach(k = 1:nrow(env_mean)) %:% 
-    foreach(i = 1:length(env), .combine = "rbind") %do% {
-      x <- env[[i]]
-      
-      b0_mean + b1_mean*x[k,] + b2_mean*(x[k,]^2)
-    }
+r_clim <-
+  foreach(k = 1:nrow(env_mean)) %:%
+  foreach(i = 1:length(env), .combine = "rbind") %do%
+  {
+    x <- env[[i]]
+
+    b0_mean + b1_mean * x[k, ] + b2_mean * (x[k, ]^2)
+  }
 
 # Climate + site level + parameter
-r_all <- foreach(i = 1:nrow(env[[1]])) %do% {
-  
-  x <- env[[i]]
-  
-  foreach(k = 1:nrow(x)) %:% 
-    foreach(h = 1:100, .combine = "rbind") %do% {
-      b0[h] + b1[h]*x[k,] + b2[h]*(x[k,]^2) + e_s[k,h]
-    }
-}
-r_all <- 
-  foreach(k = 1:nrow(env[[1]])) %:% 
-    foreach(i = 1:length(env), .combine = "rbind") %do% {
-      r_all[[i]][[k]]
-    }
+r_all <- foreach(i = 1:nrow(env[[1]])) %do%
+  {
+    x <- env[[i]]
+
+    foreach(k = 1:nrow(x)) %:%
+      foreach(h = 1:100, .combine = "rbind") %do%
+      {
+        b0[h] + b1[h] * x[k, ] + b2[h] * (x[k, ]^2) + e_s[k, h]
+      }
+  }
+r_all <-
+  foreach(k = 1:nrow(env[[1]])) %:%
+  foreach(i = 1:length(env), .combine = "rbind") %do%
+  {
+    r_all[[i]][[k]]
+  }
 
 
 # Save results in csv
@@ -149,10 +152,8 @@ dir.create(folder)
 
 sites <- unique(row.names(env[[1]]))
 for (i in 1:length(sites)) {
-  
   site_file <- paste(folder, "/", sites[i], ".csv", sep = "")
   write.csv(r_clim[[i]], site_file)
-  
 }
 
 
@@ -162,15 +163,15 @@ get_toe <- function(r_est) {
   y_min <- apply(r_est, 2, quantile, 0.05, na.rm = T)
   y_max <- apply(r_est, 2, quantile, 0.95, na.rm = T)
   y_mean <- apply(r_est, 2, mean, na.rm = T)
-  
+
   TOE_dec <- which(min(y_min[1:32]) > y_max[33:162])[1] %>%
     names() %>%
     as.numeric()
-  
-  TOE_inc <-  which(max(y_max[1:32]) < y_min[33:162])[1] %>%
+
+  TOE_inc <- which(max(y_max[1:32]) < y_min[33:162])[1] %>%
     names() %>%
     as.numeric()
-  
+
   if (y_mean[1] > y_mean[162]) {
     TOE_dec
   } else TOE_inc
@@ -182,104 +183,131 @@ TOE_clim <- map_dbl(r_clim, get_toe)
 TOE_all <- map_dbl(r_all, get_toe)
 
 cat_toe <- function(toe) {
-  
   if (is.na(toe)) return(">2100")
   if (toe <= 2025) return("<2025")
   if (toe > 2025 & toe <= 2050) return("2025 - 2050")
   if (toe > 2050 & toe <= 2075) return("2050 - 2075")
   if (toe > 2075 & toe <= 2100) return("2075 - 2100")
-  
 }
 
-TOE_param2 <- factor(sapply(TOE_param, cat_toe),
-                     levels = c("<2025", "2025 - 2050", "2050 - 2075",
-                               "2075 - 2100", ">2100"))
-TOE_proc2 <- factor(sapply(TOE_proc, cat_toe), 
-                    levels = c("<2025", "2025 - 2050", "2050 - 2075",
-                               "2075 - 2100", ">2100"))
-TOE_clim2 <- factor(sapply(TOE_clim, cat_toe),
-                    levels = c("<2025", "2025 - 2050", "2050 - 2075",
-                               "2075 - 2100", ">2100"))
-TOE_all2 <- factor(sapply(TOE_all, cat_toe),
-                   levels = c("<2025", "2025 - 2050", "2050 - 2075",
-                              "2075 - 2100", ">2100"))
+TOE_param2 <- factor(
+  sapply(TOE_param, cat_toe),
+  levels = c("<2025", "2025 - 2050", "2050 - 2075", "2075 - 2100", ">2100")
+)
+TOE_proc2 <- factor(
+  sapply(TOE_proc, cat_toe),
+  levels = c("<2025", "2025 - 2050", "2050 - 2075", "2075 - 2100", ">2100")
+)
+TOE_clim2 <- factor(
+  sapply(TOE_clim, cat_toe),
+  levels = c("<2025", "2025 - 2050", "2050 - 2075", "2075 - 2100", ">2100")
+)
+TOE_all2 <- factor(
+  sapply(TOE_all, cat_toe),
+  levels = c("<2025", "2025 - 2050", "2050 - 2075", "2075 - 2100", ">2100")
+)
 
 
 # Plot growth trajectories ------------------------------------------------
 
 plot_traj <- function(r_est, site_name, uncertainty_name, TOE) {
-
   theme_set(theme_bw())
-  
+
   y_min <- apply(r_est, 2, quantile, 0.05, na.rm = T)
   y_max <- apply(r_est, 2, quantile, 0.95, na.rm = T)
   y_mean <- apply(r_est, 2, mean, na.rm = T)
-  
+
   if (y_mean[1] > y_mean[162]) {
     y_thr <- y_min
   } else y_thr <- y_max
-  
-  data_plot <- data.frame(y_min = y_min,
-                          y_max = y_max,
-                          y_mean = y_mean,
-                          years = as.numeric(colnames(env[[1]])),
-                          hist = as.factor(
-                            c(rep("baseline", 32), rep("proj", 130))))
-  
+
+  data_plot <- data.frame(
+    y_min = y_min,
+    y_max = y_max,
+    y_mean = y_mean,
+    years = as.numeric(colnames(env[[1]])),
+    hist = as.factor(
+      c(rep("baseline", 32), rep("proj", 130))
+    )
+  )
+
   ggplot(data_plot) +
-    geom_ribbon(aes(x = years, ymin = y_min, ymax = y_max, fill = hist), 
-                alpha = 0.6) +
+    geom_ribbon(
+      aes(x = years, ymin = y_min, ymax = y_max, fill = hist),
+      alpha = 0.6
+    ) +
     geom_line(aes(y = y_mean, x = years)) +
-    labs(y = "Population Growth",
-         x = "Year",
-         title = paste(site_name, uncertainty_name, sep = ", ")) +
+    labs(
+      y = "Population Growth",
+      x = "Year",
+      title = paste(site_name, uncertainty_name, sep = ", ")
+    ) +
     geom_abline(intercept = min(y_thr[1:32]), slope = 0, linetype = 2) +
     geom_vline(xintercept = TOE, linetype = 2) +
     scale_fill_manual(values = c("#006BA4", "#FF800E")) +
-    theme(panel.border = element_blank(),
-          panel.grid.minor = element_blank(),
-          #axis.title.x = element_blank(),
-          plot.title = element_text(size = 11),
-          axis.title = element_text(size = 11),
-          axis.text = element_text(size = 10),
-          legend.position = "none") 
+    theme(
+      panel.border = element_blank(),
+      panel.grid.minor = element_blank(),
+      #axis.title.x = element_blank(),
+      plot.title = element_text(size = 11),
+      axis.title = element_text(size = 11),
+      axis.text = element_text(size = 10),
+      legend.position = "none"
+    )
 }
 
-g1 <- plot_traj(r_clim[[172]], "MIZU", "Climate Uncertainty", 
-                TOE = TOE_clim[172])
+g1 <- plot_traj(
+  r_clim[[172]],
+  "MIZU",
+  "Climate Uncertainty",
+  TOE = TOE_clim[172]
+)
 
-g2 <- plot_traj(r_proc_s[[172]], "MIZU", "Spatial Process Uncertainty",
-                TOE = TOE_proc[172])
+g2 <- plot_traj(
+  r_proc_s[[172]],
+  "MIZU",
+  "Spatial Process Uncertainty",
+  TOE = TOE_proc[172]
+)
 
-g3 <- plot_traj(r_param[[172]], "MIZU", "Parameter Uncertainty",
-                TOE = TOE_param[172])
+g3 <- plot_traj(
+  r_param[[172]],
+  "MIZU",
+  "Parameter Uncertainty",
+  TOE = TOE_param[172]
+)
 
-g4 <- plot_traj(r_all[[172]], "MIZU", "All Uncertainties",
-                TOE = TOE_all[172])
+g4 <- plot_traj(r_all[[172]], "MIZU", "All Uncertainties", TOE = TOE_all[172])
 
-(((g1 + theme(axis.title.x = element_blank())) / 
-    (g3 + theme(axis.title = element_blank()))) | 
-    (g2 + theme(axis.title = element_blank())) / 
+(((g1 + theme(axis.title.x = element_blank())) /
+  (g3 + theme(axis.title = element_blank()))) |
+  (g2 + theme(axis.title = element_blank())) /
     (g4 + theme(axis.title.y = element_blank()))) +
   plot_annotation(tag_levels = list(c("a", "c", "b", "d")))
 
 ggsave("fig2.pdf", width = 180, height = 180, units = "mm", dpi = 600)
 
-g5 <- plot_traj(r_clim[[68]], "CRZE", "Climate Uncertainty", 
-                TOE = TOE_clim[68])
+g5 <- plot_traj(r_clim[[68]], "CRZE", "Climate Uncertainty", TOE = TOE_clim[68])
 
-g6 <- plot_traj(r_proc_s[[68]], "CRZE", "Spatial Process Uncertainty",
-                TOE = TOE_proc[68])
+g6 <- plot_traj(
+  r_proc_s[[68]],
+  "CRZE",
+  "Spatial Process Uncertainty",
+  TOE = TOE_proc[68]
+)
 
-g7 <- plot_traj(r_param[[68]], "CRZE", "Parameter Uncertainty",
-                TOE = TOE_param[68])
+g7 <- plot_traj(
+  r_param[[68]],
+  "CRZE",
+  "Parameter Uncertainty",
+  TOE = TOE_param[68]
+)
 
-g8 <- plot_traj(r_all[[68]], "CRZE", "All Uncertainties",
-                TOE = TOE_all[68])
+g8 <- plot_traj(r_all[[68]], "CRZE", "All Uncertainties", TOE = TOE_all[68])
 
-(((g5 + theme(axis.title.x = element_blank())) / 
-    (g7 + theme(axis.title = element_blank()))) | 
-    (g6 + theme(axis.title = element_blank())) / 
+(((g5 + theme(axis.title.x = element_blank())) /
+  (g7 + theme(axis.title = element_blank()))) |
+  (g6 + theme(axis.title = element_blank())) /
     (g8 + theme(axis.title.y = element_blank()))) +
   plot_annotation(tag_levels = list(c("a", "c", "b", "d")))
 
@@ -293,7 +321,7 @@ ggsave("fig_traj.pdf", width = 120, height = 90, units = "mm", dpi = 600)
 
 antarctic <- st_as_sf(maps::map('world', plot = FALSE, fill = TRUE))
 idx <- which(antarctic$ID == "Antarctica")
-antarctic <- antarctic[idx,]
+antarctic <- antarctic[idx, ]
 antarctic <- st_transform(antarctic, 3031)
 
 sites_adpe_sf <- st_transform(sites_adpe_sf, 3031)
@@ -301,82 +329,111 @@ sites_adpe_sf <- st_transform(sites_adpe_sf, 3031)
 m1 <- ggplot() +
   geom_sf(data = antarctic, alpha = 0.5) +
   annotation_scale() +
-  geom_sf(data = sites_adpe_sf, aes(col = TOE_clim2), alpha = 0.5, 
-          show.legend = T) +
-  scale_color_manual(values = c("<2025" = "#009E73", 
-                                "2025 - 2050" = "#56B4E9", 
-                                "2050 - 2075" = "#F0E442",
-                                "2075 - 2100" = "#E69F00",
-                                ">2100" = "#D55E00"),
-                     drop = FALSE) +
+  geom_sf(
+    data = sites_adpe_sf,
+    aes(col = TOE_clim2),
+    alpha = 0.5,
+    show.legend = T
+  ) +
+  scale_color_manual(
+    values = c(
+      "<2025" = "#009E73",
+      "2025 - 2050" = "#56B4E9",
+      "2050 - 2075" = "#F0E442",
+      "2075 - 2100" = "#CC79A7", #"#E69F00",
+      ">2100" = "#D55E00"
+    ),
+    drop = FALSE
+  ) +
   labs(col = "TOE", title = "Climate Uncertainty") +
   coord_sf(label_axes = list(bottom = "E", left = "E")) +
-  theme(legend.position = "bottom",
-        plot.title = element_text(size = 11)) +
-  guides(colour = guide_legend(override.aes = list(size = 3))) 
+  theme(legend.position = "bottom", plot.title = element_text(size = 11)) +
+  guides(colour = guide_legend(override.aes = list(size = 3)))
 
 m2 <- ggplot() +
   geom_sf(data = antarctic, alpha = 0.5) +
   annotation_scale() +
-  geom_sf(data = sites_adpe_sf, aes(col = TOE_proc2), alpha = 0.5, 
-          show.legend = T) +
-  scale_color_manual(values = c("<2025" = "#009E73", 
-                                "2025 - 2050" = "#56B4E9", 
-                                "2050 - 2075" = "#F0E442",
-                                "2075 - 2100" = "#E69F00",
-                                ">2100" = "#D55E00"),
-                     drop = FALSE) +
+  geom_sf(
+    data = sites_adpe_sf,
+    aes(col = TOE_proc2),
+    alpha = 0.5,
+    show.legend = T
+  ) +
+  scale_color_manual(
+    values = c(
+      "<2025" = "#009E73",
+      "2025 - 2050" = "#56B4E9",
+      "2050 - 2075" = "#F0E442",
+      "2075 - 2100" = "#CC79A7", #"#E69F00",
+      ">2100" = "#D55E00"
+    ),
+    drop = FALSE
+  ) +
   labs(col = "TOE", title = "Spatial Process Uncertainty") +
   coord_sf(label_axes = list(bottom = "E", left = "E")) +
-  theme(legend.position = "bottom",
-        plot.title = element_text(size = 11)) +
-  guides(colour = guide_legend(override.aes = list(size = 3))) 
+  theme(legend.position = "bottom", plot.title = element_text(size = 11)) +
+  guides(colour = guide_legend(override.aes = list(size = 3)))
 
 m3 <- ggplot() +
   geom_sf(data = antarctic, alpha = 0.5) +
   annotation_scale() +
-  geom_sf(data = sites_adpe_sf, aes(col = TOE_param2), alpha = 0.5, 
-          show.legend = T) +
-  scale_color_manual(values = c("<2025" = "#009E73", 
-                                "2025 - 2050" = "#56B4E9", 
-                                "2050 - 2075" = "#F0E442",
-                                "2075 - 2100" = "#E69F00",
-                                ">2100" = "#D55E00"),
-                     drop = FALSE) +
+  geom_sf(
+    data = sites_adpe_sf,
+    aes(col = TOE_param2),
+    alpha = 0.5,
+    show.legend = T
+  ) +
+  scale_color_manual(
+    values = c(
+      "<2025" = "#009E73",
+      "2025 - 2050" = "#56B4E9",
+      "2050 - 2075" = "#F0E442",
+      "2075 - 2100" = "#CC79A7", #"#E69F00",
+      ">2100" = "#D55E00"
+    ),
+    drop = FALSE
+  ) +
   labs(col = "TOE", title = "Parameter Uncertainty") +
   coord_sf(label_axes = list(bottom = "E", left = "E")) +
-  theme(legend.position = "bottom",
-        plot.title = element_text(size = 11)) +
-  guides(colour = guide_legend(override.aes = list(size = 3))) 
+  theme(legend.position = "bottom", plot.title = element_text(size = 11)) +
+  guides(colour = guide_legend(override.aes = list(size = 3)))
 
 m4 <- ggplot() +
   geom_sf(data = antarctic, alpha = 0.5) +
   annotation_scale() +
-  geom_sf(data = sites_adpe_sf, aes(col = TOE_all2), alpha = 0.5, 
-          show.legend = T) +
-  scale_color_manual(values = c("<2025" = "#009E73", 
-                                "2025 - 2050" = "#56B4E9", 
-                                "2050 - 2075" = "#F0E442",
-                                "2075 - 2100" = "#E69F00",
-                                ">2100" = "#D55E00"),
-                     drop = FALSE) +
+  geom_sf(
+    data = sites_adpe_sf,
+    aes(col = TOE_all2),
+    alpha = 0.5,
+    show.legend = T
+  ) +
+  scale_color_manual(
+    values = c(
+      "<2025" = "#009E73",
+      "2025 - 2050" = "#56B4E9",
+      "2050 - 2075" = "#F0E442",
+      "2075 - 2100" = "#CC79A7", #"#E69F00",
+      ">2100" = "#D55E00"
+    ),
+    drop = FALSE
+  ) +
   labs(col = "TOE", title = "All Uncertainties") +
   coord_sf(label_axes = list(bottom = "E", left = "E")) +
-  theme(legend.position = "bottom",
-        plot.title = element_text(size = 11)) +
-  guides(colour = guide_legend(override.aes = list(size = 3))) 
+  theme(legend.position = "bottom", plot.title = element_text(size = 11)) +
+  guides(colour = guide_legend(override.aes = list(size = 3)))
 
-(m1 | m2) / (m3 | m4) / guide_area() +
+(m1 | m2) /
+  (m3 | m4) /
+  guide_area() +
   plot_annotation(tag_levels = "a") +
-  plot_layout(guides = "collect",
-              heights = c(5, 5, 1))
+  plot_layout(guides = "collect", heights = c(5, 5, 1))
 ggsave("fig4.pdf", width = 180, height = 180, units = "mm", dpi = 600)
 
 
 # Plot sea ice trajectories -----------------------------------------------
 
 ice <- env <- readRDS("data/data_coupled_transformed_adpe.rds")
-ice_mizu <- map_dfr(ice, function(x) x[which(rownames(x) == "MIZU"),]) %>%
+ice_mizu <- map_dfr(ice, function(x) x[which(rownames(x) == "MIZU"), ]) %>%
   pivot_longer(everything(), names_to = "year", values_to = "ice") %>%
   add_column(member = rep(1:50, each = 162)) %>%
   mutate(year = as.numeric(year))
@@ -386,14 +443,14 @@ ggplot(data = ice_mizu) +
   scale_colour_gradient_tableau('Blue') +
   scale_x_continuous(breaks = c(seq(1900, 2100, 50))) +
   labs(x = "Year", y = "Sea Ice Concentration") +
-  theme(panel.border = element_blank(),
-        panel.grid.minor = element_blank(),
-        #axis.title.x = element_blank(),
-        plot.title = element_text(size = 11),
-        axis.title = element_text(size = 11),
-        axis.text = element_text(size = 10),
-        legend.position = "none")
+  theme(
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank(),
+    #axis.title.x = element_blank(),
+    plot.title = element_text(size = 11),
+    axis.title = element_text(size = 11),
+    axis.text = element_text(size = 10),
+    legend.position = "none"
+  )
 
 ggsave("fig_ice.pdf", width = 120, height = 90, units = "mm", dpi = 600)
-  
-  
--- scripts/get_starting_pop_size.R
+++ scripts/get_starting_pop_size.R
@@ -1,4 +1,3 @@
-
 library(mapppdr)
 library(tidyverse)
 
@@ -9,21 +8,24 @@ z <- mapppdr::penguin_obs %>%
 
 idx <- which(z$type == "nests")
 z2 <- z
-z2$count[idx] <- z2$count[idx]*2
+z2$count[idx] <- z2$count[idx] * 2
 
 idx2 <- which(z$type == "chicks")
 z2$count[idx2] <- NA
 
 z3 <- z2 %>%
   group_by(site_id) %>%
-  summarise(median = round(median(count, na.rm = T), 2),
-            mean = round(mean(count, na.rm = T), 2),
-            min = round(min(count, na.rm = T), 2),
-            max = round(max(count, na.rm = T), 2),
-            sd = round(sd(count, na.rm = T), 2),
-            last_year = max(year)) %>%
+  summarise(
+    median = round(median(count, na.rm = T), 2),
+    mean = round(mean(count, na.rm = T), 2),
+    min = round(min(count, na.rm = T), 2),
+    max = round(max(count, na.rm = T), 2),
+    sd = round(sd(count, na.rm = T), 2),
+    last_year = max(year),
+    median_year = round(median(year))
+  ) %>%
   as.data.frame()
 
-z4 <- left_join(sites, z3, by = "site_id")  
-  
+z4 <- left_join(sites, z3, by = "site_id")
+
 write.csv(z4, "data/colony_info_adelie.csv", row.names = F)
